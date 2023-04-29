92f827e26c5fbe91b354b7ada33baeb3a6cc89f8
ayush1017 <95144833+ayush1017@users.noreply.github.com>
2023-04-29 22:48:53 +0530

    Add files via upload

--- Quiz_Application/Quiz_main.java
+++ Quiz_Application/Quiz_main.java
@@ -2,9 +2,10 @@ package Quiz_Application;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.io.IOException;
 
 public class Quiz_main extends JFrame implements ActionListener{
- String questions[][] = new String[6][5];
+ String questions[][] = new String[6][6];
  String answers[][] = new String[6][2];
  String useranswers[][] = new String[6][1];
  JLabel qno, que;   
@@ -14,6 +15,7 @@ ButtonGroup grp;
 public static int ans_given = 0;
 public static int count = 0;
 public static int score = 0;
+int k=0;
 String name;
  Quiz_main(String name)
 {
@@ -42,109 +44,119 @@ String name;
      qno.setForeground(Color.WHITE);
     add(que);
    //QUESTIONS
+    questions[0][0] = "Which is used to find and fix bugs in the Java programs.?";
+    questions[0][1] = "JVM";
+    questions[0][2] = "JDB";
+    questions[0][3] = "JDK";
+    questions[0][4] = "JRE";
+ 
+    questions[1][0] = "which film got the Oscars for best Original song?";
+    questions[1][1] = "RRR";
+    questions[1][2] = "Nomadland";
+    questions[1][3] = "Wednesday";
+    questions[1][4] = "Money Heist";
+ 
+    questions[2][0] = "Who was the second man to step on the moon?";
+    questions[2][1] = "Buzz Aldrin";
+    questions[2][2] = "Yuri Gagarin";
+    questions[2][3] = "Neil Armstrong";
+    questions[2][4] = "Michel Collins";
+ 
+    questions[3][0] = "Recently Which Country Became The 31st country of NATO?";
+    questions[3][1] = "Finland";
+    questions[3][2] = "Belarus";
+    questions[3][3] = "England";
+    questions[3][4] = "Ireland";
+ 
+    questions[4][0] = "Complete this Film Line _____ Bahubali Yani Mein?";
+    questions[4][1] = "Amrendra";
+    questions[4][2] = "Dharmendra";
+    questions[4][3] = "Mahendra";
+    questions[4][4] = "Surendra";
+ 
+    questions[5][0] = "Who is known as the Father of Indian Space program.?";
+    questions[5][1] = "Vikram Sarabhai";
+    questions[5][2] = "APJ Abdul Kalam";
+    questions[5][3] = "Homi Bhabha";
+    questions[5][4] = "CV Raman";
+
+    
+ 
+    answers[0][1] = "JDB";
+    answers[1][1] = "RRR";
+    answers[2][1] = "Buzz Aldrin";
+    answers[3][1] = "Finland";
+    answers[4][1] = "Amrendra";
+    answers[5][1] = "Vikram Sarabhai";
    
-   questions[0][0] = "Which is used to find and fix bugs in the Java programs.?";
-   questions[0][1] = "JVM";
-   questions[0][2] = "JDB";
-   questions[0][3] = "JDK";
-   questions[0][4] = "JRE";
 
-   questions[1][0] = "which film got the Oscars for best Original song?";
-   questions[1][1] = "RRR";
-   questions[1][2] = "Nomadland";
-   questions[1][3] = "Wednesday";
-   questions[1][4] = "Money Heist";
-
-   questions[2][0] = "Who was the second man to step on the moon?";
-   questions[2][1] = "Buzz Aldrin";
-   questions[2][2] = "Yuri Gagarin";
-   questions[2][3] = "Neil Armstrong";
-   questions[2][4] = "Michel Collins";
-
-   questions[3][0] = "Recently Which Country Became The 31st country of NATO?";
-   questions[3][1] = "Finland";
-   questions[3][2] = "Belarus";
-   questions[3][3] = "England";
-   questions[3][4] = "Ireland";
-
-   questions[4][0] = "Complete this Film Line _____ Bahubali Yani Mein?";
-   questions[4][1] = "Amrendra";
-   questions[4][2] = "Dharmendra";
-   questions[4][3] = "Mahendra";
-   questions[4][4] = "Surendra";
-
-   questions[5][0] = "Who is known as the Father of Indian Space program.?";
-   questions[5][1] = "Vikram Sarabhai";
-   questions[5][2] = "APJ Abdul Kalam";
-   questions[5][3] = "Homi Bhabha";
-   questions[5][4] = "CV Raman";
-
-   answers[0][1] = "JDB";
-   answers[1][1] = "RRR";
-   answers[2][1] = "Buzz Aldrin";
-   answers[3][1] = "Finland";
-   answers[4][1] = "Amrendra";
-   answers[5][1] = "Vikram Sarabhai";
-
+   
 
     //RadioButtons
-     op1= new JRadioButton();
-     op1.setBounds(170, 520, 700, 30);
-     op1.setBackground(Color.BLUE);
-     op1.setFont(new Font("Tahema", Font.PLAIN, 15));
-     op1.setForeground(Color.WHITE);
-     add(op1);
+    op1= new JRadioButton();
+    op1.setBounds(170, 520, 700, 30);
+    op1.setBackground(Color.BLUE);
+    op1.setFont(new Font("Tahema", Font.PLAIN, 15));
+    op1.setForeground(Color.WHITE);
+    add(op1);
 
-     op2= new JRadioButton();
-     op2.setBounds(170, 560, 700, 30);
-     op2.setBackground(Color.BLUE);
-     op2.setFont(new Font("Tahema", Font.PLAIN, 15));
-     op2.setForeground(Color.WHITE);
-     add(op2);
+    op2= new JRadioButton();
+    op2.setBounds(170, 560, 700, 30);
+    op2.setBackground(Color.BLUE);
+    op2.setFont(new Font("Tahema", Font.PLAIN, 15));
+    op2.setForeground(Color.WHITE);
+    add(op2);
 
-     op3= new JRadioButton();
-     op3.setBounds(170, 600, 700, 30);
-     op3.setBackground(Color.BLUE);
-     op3.setFont(new Font("Tahoma", Font.PLAIN, 15));
-     op3.setForeground(Color.WHITE);
-     add(op3);
+    op3= new JRadioButton();
+    op3.setBounds(170, 600, 700, 30);
+    op3.setBackground(Color.BLUE);
+    op3.setFont(new Font("Tahoma", Font.PLAIN, 15));
+    op3.setForeground(Color.WHITE);
+    add(op3);
 
-     op4= new JRadioButton();
-     op4.setBounds(170, 640, 700, 30);
-     op4.setBackground(Color.BLUE);
-     op4.setFont(new Font("Tahoma", Font.PLAIN, 15));
-     op4.setForeground(Color.WHITE);
-     add(op4);
-    //Grouping the radiobuttons
-      grp=new ButtonGroup();
-     grp.add(op1);
-     grp.add(op2);
-     grp.add(op3);
-     grp.add(op4);
-
-
-      next= new JButton("Next");
-      next.setBounds(1100,700,200,40);
-      next.setBackground(Color.RED);
-      next.setFont(new Font("Tahoma", Font.PLAIN, 15));
-      next.setForeground(Color.WHITE);
-      next.addActionListener(this);
-      add(next);
-
-      Submit= new JButton("Submit");
-      Submit.setBounds(1100,750,200,40);
-      Submit.setBackground(Color.RED);
-      Submit.setFont(new Font("Tahoma", Font.PLAIN, 15));
-      Submit.setForeground(Color.WHITE);
-      Submit.setEnabled(false);
-      Submit.addActionListener(this);
-      add(Submit);
-
-     
+    op4= new JRadioButton();
+    op4.setBounds(170, 640, 700, 30);
+    op4.setBackground(Color.BLUE);
+    op4.setFont(new Font("Tahoma", Font.PLAIN, 15));
+    op4.setForeground(Color.WHITE);
+    add(op4);
+   //Grouping the radiobuttons
+     grp=new ButtonGroup();
+    grp.add(op1);
+    grp.add(op2);
+    grp.add(op3);
+    grp.add(op4);
 
 
+     next= new JButton("Next");
+     next.setBounds(1100,700,200,40);
+     next.setBackground(Color.RED);
+     next.setFont(new Font("Tahoma", Font.PLAIN, 15));
+     next.setForeground(Color.WHITE);
+     next.addActionListener(this);
+     add(next);
 
-     start(count);
+     Submit= new JButton("Submit");
+     Submit.setBounds(1100,750,200,40);
+     Submit.setBackground(Color.RED);
+     Submit.setFont(new Font("Tahoma", Font.PLAIN, 15));
+     Submit.setForeground(Color.WHITE);
+     Submit.setEnabled(false);
+     Submit.addActionListener(this);
+     add(Submit);
+
+    
+    
+
+
+
+    start(count);
+
+    
+   
+
+
+    
 
     
 }
@@ -193,8 +205,9 @@ else if (ae.getSource() == Submit) {
     }
     setVisible(false);
    new Score(name, score);
-
 }
+
+  
         
 }   
  
--- Quiz_Application/Score.java
+++ Quiz_Application/Score.java
@@ -4,9 +4,16 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import java.io.IOException;
+import java.lang.ProcessBuilder;
+
+
+
 public class Score extends JFrame implements ActionListener{
 
     JButton play;
+    String name;
+    int score;
     
      Score(String name, int score)
     {
@@ -28,11 +35,11 @@ public class Score extends JFrame implements ActionListener{
         score1.setForeground(Color.WHITE);
         add(score1);
 
-        JButton play= new JButton("Play Again");
+     play= new JButton("Play Again");
         play.setBounds(500, 210, 200, 30);
         play.setFont(new Font("Tahoma", Font.PLAIN, 26));
-        play.addActionListener(this);
         play.setBackground(Color.GREEN);
+        play.addActionListener(this);
         add(play);
 
 
@@ -41,6 +48,39 @@ public class Score extends JFrame implements ActionListener{
         JLabel image= new JLabel(i1);
         image.setBounds(0, 0, 950, 850);
         add(image);
+
+       
+
+        if(score==60)
+        {
+           try{
+            ProcessBuilder pb=new ProcessBuilder("C:\\Program Files\\Windows Media Player\\wmplayer.exe", "C:\\Users\\Ayush Singh\\Downloads\\W.mp4");
+            pb.start();
+           }catch(IOException e)
+           {
+            e.printStackTrace();
+           }
+        }
+        else if(score==50)
+        {
+            JOptionPane.showMessageDialog(score1, "Wah Kya Baat hai you won  6 lakhs 40 thousand");
+        }
+        else if(score==40)
+        {
+            JOptionPane.showMessageDialog(score1, "Wah Kya Baat hai you won  3 lakhs 60 thousand");
+        }
+        else if(score==30)
+        {
+            JOptionPane.showMessageDialog(score1, "Wah Kya Baat hai you won  40 thousand");
+        }
+        else if(score==20)
+        {
+            JOptionPane.showMessageDialog(score1, "Wah Kya Baat hai you won  10,000");
+        }
+        else
+        {
+            JOptionPane.showMessageDialog(score1, "DURBHAGYA WARSH AAP DHANRASHI NAHI PRAPT KARPAYE!!");
+        }
         
 
 
@@ -53,13 +93,16 @@ public class Score extends JFrame implements ActionListener{
     }
     public void actionPerformed(ActionEvent ae)
     {
-    
-        setVisible(false);
-        new Quiz_login();
-        
-        
+        if(ae.getSource()==play)
+        {
+            setVisible(false);
+            new Quiz_login();
+            
+       
+        }
+       
+       
 
-    
     }
     public static void main(String arg[])
     {
